2d24af4067991568f88e2525ac1478baca5e0811
Athanasios Karamouzis <akaramouzis@actum.de>
2021-05-07 12:49:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.orinalUrl);
-  }
-  next();
-}
-
 const express = require('express')
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/health-ag'));
+app.use(express.static(__dirname + '/dist/health-ag'));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/health-ag/' })
-}
-);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/health-ag/index.html'))
+});
 
 //test
 
